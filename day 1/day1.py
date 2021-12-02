8c3b799f5acc05bfaaf51a59d70127bb326b5bed
mrjsavage <67581750+mrjsavage@users.noreply.github.com>
2021-12-02 20:44:10 +0000

    day1

---
+++ day 1/day1.py
@@ -0,0 +1,23 @@
+import queue
+
+count = 0
+q = queue.Queue()
+with open('day1.txt', 'r') as fp:
+	last = None
+	tot = 0
+	out = 0
+	for line in fp:
+		cur = int(line)
+		q.put(cur)
+		if q.qsize() == 4:
+			out = q.get()
+			tot = sum(list(q.queue))
+		if last != None:
+			if last < tot:
+				count += 1
+			last = tot
+		elif q.qsize() == 3:
+			last = tot
+		print(last, tot, out, list(q.queue))
+		
+print(count)
